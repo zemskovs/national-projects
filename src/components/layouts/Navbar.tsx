d2de5910c8ca8ae75c480f204333f2a6e5b906c9
Alex <sasha-elahov@yandex.ru>
2020-03-28 16:05:27 +0300

    navbar in progress

--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -1,6 +1,10 @@
 import * as React from "react";
 import NavbarBase from "react-bootstrap/Navbar";
 import "./navbar.css";
+import { Form, FormControl, Button, Nav } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { t } from "../../services/TextService";
 
 type NavbarProps = {};
 
@@ -9,7 +13,7 @@ const logo = require("./logo.svg");
 export const Navbar: React.FC = props => {
 	return (
 		<div className="navbar__wrap">
-			<NavbarBase bg="white">
+			<NavbarBase bg="white" expand="sm">
 				<NavbarBase.Brand href="/" className="navbar__top">
 					<img
 						src={logo}
@@ -18,15 +22,19 @@ export const Navbar: React.FC = props => {
 						alt="Национальные проекты Чувашии"
 					/>
 				</NavbarBase.Brand>
-				{/* <NavbarBase.Toggle />
-				<NavbarBase.Collapse>
-					<Nav
-						className="justify-content-end"
-						style={{ width: "100%" }}
-					>
-						<p>jj</p>
+				<NavbarBase.Toggle aria-controls="basic-navbar-nav" />
+				<NavbarBase.Collapse id="basic-navbar-nav">
+					<Form inline>
+						<FormControl type="text" placeholder={t("Поиск")} />
+						<Button variant="outline-primary">
+							<FontAwesomeIcon icon={faSearch} />
+						</Button>
+					</Form>
+					<Nav className="justify-content-end" style={{ width: "100%" }}>
+						<Nav.Link href="#home">Home</Nav.Link>
+						<Nav.Link href="#link">Link</Nav.Link>
 					</Nav>
-				</NavbarBase.Collapse> */}
+				</NavbarBase.Collapse>
 			</NavbarBase>
 		</div>
 	);
